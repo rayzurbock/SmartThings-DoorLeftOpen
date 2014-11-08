c82fa886a40661f6ebd6e334192d64af418b3e2c
rayzurbock <brian@rayzurbock.com>
2014-11-08 17:08:05 -0600

    Formatting update
    Replaced tabs with spaces.

--- Doorleftopen.groovy
+++ Doorleftopen.groovy
@@ -5,7 +5,6 @@
  *  Code to prevent false alarms added: 11/8/2014 - Brian Lowrance - brian@rayzurbock.com
  */
 
-// Automatically generated. Make future change here.
 definition(
     name: "Door left open (with photos)",
     namespace: "",
@@ -13,54 +12,54 @@ definition(
     description: "Notify when something is left open and take some photos",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
-
+    
 preferences {
-	section("When . . .") {
-		input "contactSensor", "capability.contactSensor", title: "This is left open"
+    section("When . . .") {
+        input "contactSensor", "capability.contactSensor", title: "This is left open"
         input "numMinutes", "number", title: "For how many minutes", required: false
         input "messageText", "text", title: "Send notification that says", required: false
         input "phoneNumber", "phone", title: "Send SMS message to", required: false
-	}
+    }
     section("Take photos with"){
-    	input "camera", "capability.imageCapture", required: false
-		input "burstCount", "number", title: "How many? (default 5)", defaultValue:5, required: false
+        input "camera", "capability.imageCapture", required: false
+        input "burstCount", "number", title: "How many? (default 5)", defaultValue:5, required: false
     }
 }
 
 def installed() {
-	subscribe(contactSensor, "contact", onContactChange);
+    subscribe(contactSensor, "contact", onContactChange);
 }
 
 def updated() {
-	unsubscribe()
-   	subscribe(contactSensor, "contact", onContactChange);
+    unsubscribe()
+    subscribe(contactSensor, "contact", onContactChange);
 }
 
 def onContactChange(evt) {
-	log.debug "onContactChange";
-	if (evt.value == "open") {
-    	runIn(numMinutes * 60, onContactLeftOpenHandler);
+    log.debug "onContactChange";
+    if (evt.value == "open") {
+        runIn(numMinutes * 60, onContactLeftOpenHandler);
     } else {
-    	unschedule(onContactLeftOpenHandler);
+        unschedule(onContactLeftOpenHandler);
     }
 }
 
 def onContactLeftOpenHandler() {
-	log.debug "onContactLeftOpenHandler";
+    log.debug "onContactLeftOpenHandler";
     if (contactSensor.latestValue == "open") {
-       log.debug "Door still open, alert"
-	   sendPush(messageText);
-       sendSms(phoneNumber, messageText);
-       if (camera) {
-		   camera.take()
-           log.debug "Camera: Snap"
-		   (1..((burstCount ?: 10) - 1)).each {
-			   log.debug "Camera: Snap"
-			   camera.take(delay: (500 * it))
-			   pause(1500)
-		   }
-	   }
+        log.debug "Door still open, alert"
+        sendPush(messageText);
+        sendSms(phoneNumber, messageText);
+        if (camera) {
+            camera.take()
+            log.debug "Camera: Snap"
+            (1..((burstCount ?: 10) - 1)).each {
+                log.debug "Camera: Snap"
+                camera.take(delay: (500 * it))
+                pause(1500)
+        }
+    }
     } else {
-       log.debug "Door closed, cancel alert"
+        log.debug "Door closed, cancel alert"
     }
 }
